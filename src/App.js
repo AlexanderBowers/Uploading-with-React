ccc63d6cedf4b4350ed200efe06fd489cd01702e
AlexanderBowers <7319abowers@gmail.com>
2021-11-06 15:15:52 -0700

    added state and upload handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+//import axios from 'axios'
 
 class App extends Component {
+  state = {
+    selectedFile: null
+  }
+
   fileSelectedHandler = event => {
-    console.log(event.target)
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  fileUploadHandler = () => {
+    const formData = new FormData()
+    formData.append('image', this.state.selectedFile, this.state.selectedFile.name)
+    //axios.post('insertLinkHere', formData) it seems I'd sent my regular post requests from here;
   }
   
   render() {
     return (
       <div className="App">
         <input type="file"  onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
     );
   }
